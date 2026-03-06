a99d3f09d9730169c5237c0555821aee1065c2a1
Nebi Anil Atici <anillatici@gmail.com>
2026-03-06 23:19:47 +1100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Home from './pages/Home';
